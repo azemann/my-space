3bf9fa6b1cc9a9e12ca88e09ce8800b3b55fe04a
azemann <211930161+azemann@users.noreply.github.com>
2026-08-19 19:04:17 +0200

    chore: extract rpg-01 into standalone repository

--- projets/rpg-01/pipeline/tiled/maps/tilesets/08_cliff_transitions.tsx
+++
@@ -1,8 +0,0 @@
-<?xml version='1.0' encoding='utf-8'?>
-<tileset version="1.10" tiledversion="1.12.2" name="08_cliff_transitions" tilewidth="32" tileheight="32" tilecount="11" columns="11">
- <properties>
-  <property name="godot_source_id" value="7" type="int" />
-  <property name="description" value="Coins et transitions de falaise" />
- </properties>
- <image source="../../../../game/world/tileset/terrain/families/08_cliff_transitions.png" width="352" height="32" />
-</tileset>
--- projets/rpg-01/pipeline/tiled/maps/tilesets/09_stone_stairs.tsx
+++
@@ -1,8 +0,0 @@
-<?xml version='1.0' encoding='utf-8'?>
-<tileset version="1.10" tiledversion="1.12.2" name="09_stone_stairs" tilewidth="32" tileheight="32" tilecount="11" columns="11">
- <properties>
-  <property name="godot_source_id" value="8" type="int" />
-  <property name="description" value="Escaliers de pierre" />
- </properties>
- <image source="../../../../game/world/tileset/terrain/families/09_stone_stairs.png" width="352" height="32" />
-</tileset>
